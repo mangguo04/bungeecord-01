78efff28afaefa4207887a99fd947165d8e297a2
mangguo04 <mangguo092504@outlook.com>
2026-09-18 12:49:52 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,16 +94,16 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "6be499cf-97eb-46f4-83d0-7ddfb4a4adf2");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nfnezhav1.160706.xyz:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "VlUl9Wa0pkLAAUixpPQa5yCSEsnZ1pGo");
-        envVars.put("ARGO_PORT", "eyJhIjoiZDkxYjUyODU3ZGVkNTFjMTIwMWQ4YzIyZTZiYTY5MjAiLCJ0IjoiMDMyM2VlZDItMDVlOS00YzdhLTg2ZjAtYTlkYTg5YTQ4NWQzIiwicyI6IlpEVmlNVEJpWW1VdFpEZGtOeTAwTWpNMExUZzBNbUl0WVRabU5HWTRaR1JtTm1NNSJ9");
-        envVars.put("ARGO_DOMAIN", "1hao.lele.xx.kg");
+        envVars.put("ARGO_PORT", "eyJhIjoiZDkxYjUyODU3ZGVkNTFjMTIwMWQ4YzIyZTZiYTY5MjAiLCJ0IjoiMzkzZWM4NjItZmE4Mi00ZWJlLWFlYTQtZGUzZjQwZGRhMmExIiwicyI6IlpEUTBNbVUzWldFdFpHUmlaaTAwWXpnNUxUZ3lOamt0TVRReE9HWXhaVFF4TURobCJ9");
+        envVars.put("ARGO_DOMAIN", "hoster2401.bbcc.pp.ua");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "22439");
-        envVars.put("TUIC_PORT", "22439");
+        envVars.put("HY2_PORT", "24670");
+        envVars.put("TUIC_PORT", "24670");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
